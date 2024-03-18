d21e99f626b36b17823966c1132209fbfb316426
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-03-18 18:19:56 +0100

    Build pack

--- build/lib/IHSetMillerDean/millerDean.py
+++ build/lib/IHSetMillerDean/millerDean.py
@@ -25,11 +25,11 @@ def millerDean(Hb, depthb, sl, wast, dt, Hberm, Y0, kero, kacr, Yini, flagP=1, O
 
     for i in range(1, len(Hb)):
         if Y[i] < yeq[i]:
-            # A = kacr[i] * dt * 0.5
-            Y[i] = (Y[i - 1] + kacr[i] * dt * 0.5 * (yeq[i] + yeq[i - 1] - Y[i - 1])) / (1 + kacr[i] * dt * 0.5)
+            A = kacr[i] * dt * 0.5
+            Y[i] = (Y[i - 1] + A * (yeq[i] + yeq[i - 1] - Y[i - 1])) / (1 + A)
         else:
-            # A = kero[i] * dt * 0.5
-            Y[i] = (Y[i - 1] + kero[i] * dt * 0.5 * (yeq[i] + yeq[i - 1] - Y[i - 1])) / (1 + kero[i] * dt * 0.5)
+            A = kero[i] * dt * 0.5
+            Y[i] = (Y[i - 1] + A * (yeq[i] + yeq[i - 1] - Y[i - 1])) / (1 + A)
 
     return Y
 
