5c20947534c98f32cede845340b3f8c4be258141
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-03-14 12:44:39 +0100

    naming model

--- IHSetMillerDean/calibration_2.py
+++ IHSetMillerDean/calibration_2.py
@@ -26,6 +26,7 @@ class cal_MillerDean_2(object):
         data = xr.open_dataset(path)
         
         cfg = json.loads(data.attrs['MillerDean'])
+        self.cfg = cfg
 
         self.cal_alg = cfg['cal_alg']
         self.metrics = cfg['metrics']
